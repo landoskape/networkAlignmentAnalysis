406ea97ff35b5eaaf2cdfaa67a084bce01452587
Andrew Landau <andrewtylerlandau@gmail.com>
2023-12-15 14:26:35 +0000

    First MLP network is ready

--- networkAlignmentAnalysis/utils.py
+++ networkAlignmentAnalysis/utils.py
@@ -3,7 +3,7 @@ import numpy as np
 import torch.nn.functional as F
 from torchvision import transforms
     
-@torch.no_grad()
+
 def alignment(input, weight, method='alignment'):
     """
     measure alignment (proportion variance explained) between **input** and **weight**
@@ -47,71 +47,71 @@ def alignment(input, weight, method='alignment'):
     # proportion of variance explained by a projection of the input onto each weight
     return rq/torch.trace(cc)
 
-@torch.no_grad()
-def alignment_linear(activity, weight, method='alignment'):
-    return alignment(activity, weight, method=method)
 
-@torch.no_grad()
+def alignment_linear(activity, layer, method='alignment'):
+    return alignment(activity, layer.weight.data, method=method)
+
+
+
+
+
+# alignment methods related to convolution -- need work!
+#
 def alignment_convolutional(activity, weight, each_look=True):
     h_max, w_max = get_maximum_strides(activity.shape[2], activity.shape[3], weight)
     
-# similarity / alignment for convolutional network layers
-@torch.no_grad()
-def alignmentConvLayer(inputActivity, layer, eachLook=True):
-    hMax, wMax = getMaximumStrides(inputActivity.shape[2], inputActivity.shape[3], layer)
-    if eachLook:
-        preprocess0 = transforms.Pad(layer.padding)
-        pInput0 = preprocess0(inputActivity).detach()
-        numLooks = hMax * wMax
-        numChannels = layer.out_channels
-        alignLayer = torch.empty((numChannels, numLooks))
-        for h in range(hMax): 
-            for w in range(wMax):
-                alignLayer[:,w+hMax*h] = alignmentConvLook(pInput0, layer, (h,w))
-        return alignLayer
-    else:
-        unfoldedInput = F.unfold(inputActivity.detach(), layer.kernel_size, stride=layer.stride, padding=layer.padding, dilation=layer.dilation)
-        unfoldedInput = unfoldedInput.transpose(1,2).reshape(inputActivity.size(0), -1)
-        unfoldedWeight = layer.weight.data.detach().view(layer.weight.size(0), -1).repeat(1, hMax*wMax)
-        return alignment(unfoldedInput, unfoldedWeight)
+# # similarity / alignment for convolutional network layers
+# def alignmentConvLayer(inputActivity, layer, eachLook=True):
+#     hMax, wMax = getMaximumStrides(inputActivity.shape[2], inputActivity.shape[3], layer)
+#     if eachLook:
+#         preprocess0 = transforms.Pad(layer.padding)
+#         pInput0 = preprocess0(inputActivity).detach()
+#         numLooks = hMax * wMax
+#         numChannels = layer.out_channels
+#         alignLayer = torch.empty((numChannels, numLooks))
+#         for h in range(hMax): 
+#             for w in range(wMax):
+#                 alignLayer[:,w+hMax*h] = alignmentConvLook(pInput0, layer, (h,w))
+#         return alignLayer
+#     else:
+#         unfoldedInput = F.unfold(inputActivity.detach(), layer.kernel_size, stride=layer.stride, padding=layer.padding, dilation=layer.dilation)
+#         unfoldedInput = unfoldedInput.transpose(1,2).reshape(inputActivity.size(0), -1)
+#         unfoldedWeight = layer.weight.data.detach().view(layer.weight.size(0), -1).repeat(1, hMax*wMax)
+#         return alignment(unfoldedInput, unfoldedWeight)
 
-@torch.no_grad()
-def alignmentConvLook(pInput, layer, stride):
-    # Take (NI, C, H, W) (transformed) input activity
-    # And compute the similarity for one "look" e.g. one position of the convolutional filter
-    numImages = pInput.shape[0]
-    wIdxStart, hIdxStart = np.meshgrid(np.arange(0,layer.kernel_size[0]), np.arange(0, layer.kernel_size[1]))
-    numElements = pInput.shape[1] * layer.kernel_size[0] * layer.kernel_size[1]
-    alignedInput = pInput[:,:,hIdxStart + stride[0]*layer.stride[0], wIdxStart + stride[1]*layer.stride[1]].reshape(numImages, numElements)
-    alignedWeights = layer.weight.data.reshape(layer.out_channels, numElements).detach()
-    return alignment(alignedInput, alignedWeights, method='alignment')
+# def alignmentConvLook(pInput, layer, stride):
+#     # Take (NI, C, H, W) (transformed) input activity
+#     # And compute the similarity for one "look" e.g. one position of the convolutional filter
+#     numImages = pInput.shape[0]
+#     wIdxStart, hIdxStart = np.meshgrid(np.arange(0,layer.kernel_size[0]), np.arange(0, layer.kernel_size[1]))
+#     numElements = pInput.shape[1] * layer.kernel_size[0] * layer.kernel_size[1]
+#     alignedInput = pInput[:,:,hIdxStart + stride[0]*layer.stride[0], wIdxStart + stride[1]*layer.stride[1]].reshape(numImages, numElements)
+#     alignedWeights = layer.weight.data.reshape(layer.out_channels, numElements).detach()
+#     return alignment(alignedInput, alignedWeights, method='alignment')
 
-@torch.no_grad()
-def similarityConvLayer(inputActivity, layer):
-    preprocess0 = transforms.Pad(layer.padding)
-    pInput0 = preprocess0(inputActivity).detach()
-    hMax, wMax = getMaximumStrides(inputActivity.shape[2], inputActivity.shape[3], layer)
-    numLooks = hMax * wMax
-    numChannels = layer.out_channels
-    simLayer = torch.empty((numChannels, numLooks))
-    for h in range(hMax): 
-        for w in range(wMax):
-            simLayer[:,w+hMax*h] = similarityConvLook(pInput0, layer, (h,w))
-    return simLayer
+# def similarityConvLayer(inputActivity, layer):
+#     preprocess0 = transforms.Pad(layer.padding)
+#     pInput0 = preprocess0(inputActivity).detach()
+#     hMax, wMax = get_maximum_strides(inputActivity.shape[2], inputActivity.shape[3], layer)
+#     numLooks = hMax * wMax
+#     numChannels = layer.out_channels
+#     simLayer = torch.empty((numChannels, numLooks))
+#     for h in range(hMax): 
+#         for w in range(wMax):
+#             simLayer[:,w+hMax*h] = similarityConvLook(pInput0, layer, (h,w))
+#     return simLayer
 
-@torch.no_grad()
-def similarityConvLook(pInput, layer, stride):
-    # Take (NI, C, H, W) (transformed) input activity
-    # And compute the similarity for one "look" e.g. one position of the convolutional filter
-    numImages = pInput.shape[0]
-    wIdxStart, hIdxStart = np.meshgrid(np.arange(0,layer.kernel_size[0]), np.arange(0, layer.kernel_size[1]))
-    numElements = pInput.shape[1] * layer.kernel_size[0] * layer.kernel_size[1]
-    alignedInput = pInput[:,:,hIdxStart + stride[0]*layer.stride[0], wIdxStart + stride[1]*layer.stride[1]].reshape(numImages, numElements)
-    alignedWeights = layer.weight.data.reshape(layer.out_channels, numElements).detach()
-    return alignment(alignedInput, alignedWeights, method='similarity')
+# def similarityConvLook(pInput, layer, stride):
+#     # Take (NI, C, H, W) (transformed) input activity
+#     # And compute the similarity for one "look" e.g. one position of the convolutional filter
+#     numImages = pInput.shape[0]
+#     wIdxStart, hIdxStart = np.meshgrid(np.arange(0,layer.kernel_size[0]), np.arange(0, layer.kernel_size[1]))
+#     numElements = pInput.shape[1] * layer.kernel_size[0] * layer.kernel_size[1]
+#     alignedInput = pInput[:,:,hIdxStart + stride[0]*layer.stride[0], wIdxStart + stride[1]*layer.stride[1]].reshape(numImages, numElements)
+#     alignedWeights = layer.weight.data.reshape(layer.out_channels, numElements).detach()
+#     return alignment(alignedInput, alignedWeights, method='similarity')
 
-@torch.no_grad()
-def getMaximumStrides(hInput, wInput, layer):
+def get_maximum_strides(hInput, wInput, layer):
     hMax = int(np.floor((hInput + 2*layer.padding[0] - layer.dilation[0]*(layer.kernel_size[0] - 1) -1)/layer.stride[0] + 1))
     wMax = int(np.floor((wInput + 2*layer.padding[1] - layer.dilation[1]*(layer.kernel_size[1] - 1) -1)/layer.stride[1] + 1))
     return hMax, wMax
