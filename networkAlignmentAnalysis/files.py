892e4e61051d54ab87d0fea632e2b4d9fa546fc6
Andrew Landau <andrewtylerlandau@gmail.com>
2023-12-17 16:02:35 +0000

    Organized file management

--- networkAlignmentAnalysis/files.py
+++ networkAlignmentAnalysis/files.py
@@ -1,4 +1,16 @@
-import os
+from pathlib import Path
+
+def local_path():
+    return Path('C:/Users/andrew/Documents/machineLearning')
+
+def data_path():
+    return local_path() / 'datasets'
+
+def dataset_path(dataset):
+    """path to stored dataset"""
+    if dataset=='MNIST':
+        # 
+        return data_path()
 
 def getDataPath(dataset='MNIST'):
     # Path to stored datasets (might add input argument for running on a different computer...)
