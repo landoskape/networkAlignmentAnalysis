e10d1fa48e1d28a25d06bb1623bf79231107a439
Celia <celiaberon@gmail.com>
2024-01-28 17:50:46 -0500

    dealing with network depending hostname

--- networkAlignmentAnalysis/files.py
+++ networkAlignmentAnalysis/files.py
@@ -4,8 +4,7 @@ from pathlib import Path
 
 PATH_REGISTRY = {
     'DESKTOP-M2J64J2': Path('C:/Users/andrew/Documents/machineLearning'),
-    'Celias-MBP.lan': Path('/Users/celiaberon/Documents/machine_learning'),
-    'celias-mbp.harvardsecure.wireless.med.harvard.edu': Path('/Users/celiaberon/Documents/machine_learning'),
+    'Celia': Path('/Users/celiaberon/Documents/machine_learning'),
     'cberon': Path('/n/home00/cberon/alignment/')
 }
 
@@ -18,6 +17,8 @@ def get_username():
 def local_path():
     """method for defining the local root path for datasets and results"""
     hostname = get_hostname()
+    if hostname.lower().startswith('celia'):
+        hostname = 'Celia'
     hostname = hostname if hostname in PATH_REGISTRY else get_username()
     if hostname not in PATH_REGISTRY:
         raise ValueError(f"hostname ({hostname}) is not registered in the path registry")
