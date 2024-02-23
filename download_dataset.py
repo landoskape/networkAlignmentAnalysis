a2f03557e61258efd8a739f35bd2e5dbe0aaa79d
Andrew Landau <andrewtylerlandau@gmail.com>
2024-02-23 12:46:00 +0000

    add correct description

--- download_dataset.py
+++ download_dataset.py
@@ -2,7 +2,7 @@ from networkAlignmentAnalysis.datasets import get_dataset
 from argparse import ArgumentParser
 
 def get_args(args=None):
-    parser = ArgumentParser(description='test alignment code')
+    parser = ArgumentParser(description='simple program for downloading a dataset to the local file location')
     parser.add_argument('--dataset', type=str, default='MNIST')
     return parser.parse_args(args=args)
 
