7aa7de79b0a5782d5b579cccdd7e857f805a19cd
landoskape <andrewtylerlandau@gmail.com>
2024-03-05 03:11:50 -0500

    organizing and preparing good cluster code

--- cluster/imagenet-example/imagenet.py
+++ cluster/imagenet-example/imagenet.py
@@ -1,4 +1,5 @@
 import argparse
+import wandb
 import torch
 import torch.nn.functional as F
 import torch.optim as optim
@@ -18,7 +19,21 @@ from networkAlignmentAnalysis import datasets
 from networkAlignmentAnalysis.models.registry import get_model
 
 
-def train(args, model, device, dataset, optimizer, epoch, rank, train=True):
+def configure_wandb(args):
+    """create a wandb run file and set environment parameters appropriately"""
+    if args.use_wandb:
+        wandb.login()
+        run = wandb.init(
+            project=args.job_name,
+            name=args.job_folder,
+        )
+        os.environ["WANDB_MODE"] = "offline"
+        return run
+
+    return None
+
+
+def train(args, model, device, dataset, optimizer, epoch, rank, run, train=True):
     dataloader = dataset.train_loader if train else dataset.test_loader
     if dataset.distributed:
         if train:
@@ -46,11 +61,13 @@ def train(args, model, device, dataset, optimizer, epoch, rank, train=True):
                 print(
                     f"Train Epoch: {epoch} [{batch_idx}/{len(dataloader)} ({100.*batch_idx/len(dataloader):.0f}%)] \t Loss: {loss.item():.6f}"
                 )
+                if run is not None:
+                    run.log(dict(epoch=epoch, batch_idx=batch_idx, train_loss=loss.item()))
             if args.dry_run:
                 break
 
 
-def test(model, device, dataset, train=False):
+def test(model, device, dataset, run, train=False):
     dataloader = dataset.train_loader if train else dataset.test_loader
     model.eval()
     test_loss = 0
@@ -70,6 +87,9 @@ def test(model, device, dataset, train=False):
     print(
         f"\nTest set: Average loss: {test_loss:.4f}, Accuracy: {correct}/{attempts} ({100.*correct/attempts:.0f}%)\n"
     )
+
+    if run is not None:
+        run.log(dict(test_loss=test_loss, test_accuracy=100.0 * correct / attempts))
 
 
 def create_dataset(name, net, distributed=True, loader_parameters={}):
@@ -91,10 +111,22 @@ def main():
     # Training settings
     parser = argparse.ArgumentParser(description="PyTorch ImageNet Example")
     parser.add_argument(
-        "--model", type=str, default="AlexNet", help="which model type to use (default: AlexNet)"
+        "--job-name",
+        type=str,
+        default="ImageNet-Example",
+        help="job name for this project",
     )
     parser.add_argument(
-        "--dataset", type=str, default="ImageNet", help="which dataset to use (default: ImageNet)"
+        "--job-folder",
+        type=str,
+        default=".",
+        help="job folder for storing data",
+    )
+    parser.add_argument(
+        "--use_wandb",
+        default=False,
+        action="store_true",
+        help="if used, will log experiment to WandB",
     )
     parser.add_argument(
         "--batch-size",
@@ -114,12 +146,12 @@ def main():
         "--lr", type=float, default=1.0, metavar="LR", help="learning rate (default: 1.0)"
     )
     parser.add_argument(
-        "--wd", type=float, default=0.01, metavar="WD", help="weight decay (default: 0.01)"
+        "--wd", type=float, default=0.0, metavar="WD", help="weight decay (default: 0.0)"
     )
     parser.add_argument(
         "--gamma",
         type=float,
-        default=0.99,
+        default=0.9,
         metavar="M",
         help="Learning rate step gamma (default: 0.99)",
     )
@@ -144,6 +176,8 @@ def main():
     )
     args = parser.parse_args()
 
+    run = configure_wandb(args)
+
     torch.manual_seed(args.seed)
 
     world_size = int(os.environ["WORLD_SIZE"])
@@ -158,7 +192,7 @@ def main():
 
     loader_parameters = dict(
         batch_size=args.batch_size,
-        num_workers=int(os.environ["SLURM_CPUS_PER_TASK"]),
+        num_workers=max(int(os.environ["SLURM_CPUS_PER_TASK"]) - 1, 1),  # save a little headroom
     )
 
     if world_size > 1:
@@ -170,8 +204,8 @@ def main():
     torch.cuda.set_device(local_rank)
     print(f"host: {gethostname()}, rank: {rank}, local_rank: {local_rank}")
 
-    model_name = args.model
-    dataset_name = args.dataset
+    model_name = "AlexNet"
+    dataset_name = "ImageNet"
     net = get_model(model_name, build=True, dataset=dataset_name)
     dataset = create_dataset(
         dataset_name, net, distributed=world_size > 1, loader_parameters=loader_parameters
@@ -185,9 +219,9 @@ def main():
 
     for epoch in range(1, args.epochs + 1):
         epoch_time = time.time()
-        train(args, ddp_model, local_rank, dataset, optimizer, epoch, rank)
+        train(args, ddp_model, local_rank, dataset, optimizer, epoch, rank, run)
         if rank == 0:
-            test(ddp_model, local_rank, dataset)
+            test(ddp_model, local_rank, dataset, run)
         scheduler.step()
         epoch_time = time.time() - epoch_time
         if rank == 0:
@@ -196,7 +230,7 @@ def main():
             )
 
     if args.save_model and rank == 0:
-        torch.save(model.state_dict(), f"{model_name}_{dataset_name}.pt")
+        torch.save(model.state_dict(), os.path.join(args.job_folder, "alexnet_imagenet.pt"))
 
     if world_size > 1:
         dist.destroy_process_group()
